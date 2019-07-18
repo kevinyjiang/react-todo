513de184b031b4d3eb369e930cdf5726090b106c
kevinyjiang <kyj2108@columbia.edu>
2019-07-18 16:46:08 -0400

    start setting up backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const dbRoutes = require('./routes/databaseAccess.js');
+
+app.use(express.static('build'));
+app.use('/db', dbRoutes);
+
+app.listen(3000, () => {
+  console.log('Server for React Todo App listening on port 3000!')
+});
